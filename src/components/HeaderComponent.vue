fe5c1996005993c9341804490b2e0f462c7f9e47
Leonomics <leocv97@gmail.com>
2022-09-07 18:36:46 +0200

    install axios

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <!--<h1>{{ msg }}</h1>-->
+    <nav class="navbarStyle navbar navbar-light">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/spotify.png" width="30" height="30" alt="">
+    </a>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../styles/general.scss';
+
+  .navbarStyle{
+    background-color: #2e3a46;
+  }
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="hello">
+        <div class="container">
+    <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+    </div>
+  </template>
+  
+<script>
+
+import axios from "axios"; 
+
+  export default {
+    name: "HeaderComponent",
+    props: {
+      msg: String,
+    },
+    data(){
+    return  {
+        listDiscs: []
+    };
+    
+  },
+    created(){
+    axios
+                    .get('https://flynn.boolean.careers/exercises/api/array/music')
+                    .then((res) => {
+                console.log(res.data.response)
+                this.listDiscs = res.data;
+            });
+  },
+  };
+
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+    @import '../styles/general.scss';
+  
+    .navbarStyle{
+      background-color: #2e3a46;
+    }
+  </style>
+  
