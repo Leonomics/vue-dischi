1097a2a6fdeb25b4331364eddfed84b40ea8d7cf
Leonomics <leocv97@gmail.com>
2022-09-08 16:42:05 +0200

    minor changes

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,7 +6,7 @@
         <img src="../assets/spotify.png" width="30" height="30" alt="">
       </a>
       <select v-model="genre" class="selectpicker" aria-label="Default select example">
-        <option v-for="(genre, i) in genres" :key="i">{{genre}}</option>
+        <option v-for="(genre, i) of genres" :key="i">{{genre}}</option>
       </select>
     </nav>
   </div>
@@ -23,17 +23,25 @@ export default {
     return{
       genre: "",
       genres: ["Rock", "Pop", "Jazz", "Metal"]
-    }
+    };
 
   },
-  /*computed(){
-    filteredGenres(){
-      return this.listDisks.filter((el) => {
+  computed: {
+    filteredGenres() {
+      return this.listDisks.filter((disk) => {
+        const songGenre = disk.genre.toLowerCase()
+        const genre = this.genre.toLowerCase()
 
-      })
+        if (songGenre.includes(genre)){
+          return true
+        }
+        else{
+          return false
+        }
+      });
     }
-  }*/
-};
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
